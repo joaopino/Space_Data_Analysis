747e7c957ed62b23d8941c1755480bcec6977b00
joaopino <joaomiguelpino@gmail.com>
2025-05-19 00:33:02 +0100

    added folder for analysis and vizualizations

---
+++ Analysis_and_Visualizations/map_vizualization.py
@@ -0,0 +1,8 @@
+"""
+Auxiliary functions for supporting the visualization of clusters on the Portuguese map.
+
+This module provides helper functions to facilitate the creation, customization, and display of cluster visualizations
+over geographic representations of Portugal. The functions handle tasks such map plotting, and annotation to enhance the interpretability of clustering results in a geospatial context.
+
+Intended for use in presentation of clustering results involving Portuguese geographic data divided by municipalities.
+"""
