1643c1d4acc2add26df4ff29ff5b052d58223344
joaopino <joaomiguelpino@gmail.com>
2025-05-19 18:52:35 +0100

    Added map visualization

--- Analysis_and_Visualizations/map_vizualization.py
+++ Analysis_and_Visualizations/map_vizualization.py
@@ -6,3 +6,30 @@ over geographic representations of Portugal. The functions handle tasks such map
 
 Intended for use in presentation of clustering results involving Portuguese geographic data divided by municipalities.
 """
+import geopandas as gpd
+
+
+def add_geodata(geojson_path,dataframe):
+    """
+    Add geospatial data to a DataFrame using a GeoJSON file.
+
+    Parameters
+    ----------
+    geojson_path : str
+        Path to the GeoJSON file containing the geospatial data.
+    dataframe : pd.DataFrame
+        DataFrame to which the geospatial data will be added.
+
+    Returns
+    -------
+    gdf : gpd.GeoDataFrame
+        GeoDataFrame containing the merged data.
+    """
+    # Load the GeoJSON file
+    gdf = gpd.read_file(geojson_path)
+
+    # Merge the GeoDataFrame with the DataFrame based on 'id_municipio'
+    gdf = gdf.merge(dataframe, left_on='id_municipio', right_on='id_municipio')
+
+    return gdf
+
